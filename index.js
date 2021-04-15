f891c39c44e4f5fc04456282966826f020c2d07f
md umar faruk <mdumar11233>
2021-04-16 02:03:07 +0600

    added new data

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-const express = require('express');
+const express = require('express')
+const app = express()
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ij0ac.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const port =process.env.PORT || 5000;
+app.use(bodyParser.json())
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+
+client.connect(err => {
+  const servicesCollection = client.db("akIndustry").collection("services");
+  const reviewsCollection = client.db("akIndustry").collection("reviews");
+
+  app.post('/review', (req, res) => {
+    const review = req.body;
+    reviewsCollection.insertOne(review)
+    .then(result => {
+      res.send(result.insertedCount > 0)
+    })
+  })
+
+});
+
+
+
+app.listen(port)
+
+
+

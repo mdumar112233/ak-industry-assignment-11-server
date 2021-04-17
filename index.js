981e69522368ad2e2887b1f9a1a93746409fc611
md umar faruk <mdumar11233>
2021-04-17 12:07:07 +0600

    added new data

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const port =process.env.PORT || 5000;
-app.use(bodyParser.json())
 app.use(cors());
+app.use(bodyParser.json())
+// app.use(express.urlencoded({extended:false}));
 
 
 app.get('/', (req, res) => {
@@ -23,11 +24,42 @@ client.connect(err => {
   const servicesCollection = client.db("akIndustry").collection("services");
   const reviewsCollection = client.db("akIndustry").collection("reviews");
 
+  app.post('/services', (req, res) => {
+    const services = req.body;
+    servicesCollection.insertOne(services)
+    .then(result => {
+      console.log(result);
+      res.send(result.insertedCount > 0);
+    })
+  })
+
   app.post('/review', (req, res) => {
     const review = req.body;
     reviewsCollection.insertOne(review)
     .then(result => {
       res.send(result.insertedCount > 0)
+    })
+  })
+
+  app.get('/serviceData', (req, res) => {
+    servicesCollection.find({})
+    .toArray((err, documents) => {
+      res.send(documents)
+    })
+  })
+
+  app.get('/reviewData', (req, res)=> {
+    reviewsCollection.find({})
+    .toArray((err, documents) => {
+      res.send(documents)
+    })
+  })
+
+  app.get('/serviceInfo/:id', (req, res) => {
+    const id = req.params.id;
+    servicesCollection.find({name: id})
+    .toArray((err, documents) => {
+      res.send(documents)
     })
   })
 
